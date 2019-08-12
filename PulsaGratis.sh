ea4631c678ec05150cdc6df4b6a5410b389efacc
MrN0name <53706015+MrN0nam3@users.noreply.github.com>
2019-08-12 15:01:25 +0700

    Add files via upload

---
+++ PulsaGratis.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+#mau ngapain cill?
+#mau recode awowkok
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+bash jr.sh
+echo
+clear
+echo  $red
+echo 'Memuat......';
+sleep 2.0;
+clear
+echo  $red
+echo 'Sabar Ye Tod!!...####... Prosses Sedang Berjalan.. ###...';
+sleep 3.0;
+clear
+echo  $red
+echo 'Sabar Tod!.............................';
+sleep 2.0;
+echo  $green
+echo "Processing 50%";
+sleep 1.0;
+clear
+echo  $red
+echo  'Udah Di Bilangin Sabar Anjenk...............';
+sleep 2.0;
+echo  $green
+echo 'Processing 80%...';
+sleep 1.0;
+clear
+echo  $red
+echo 'Loading.........88%.......................';
+sleep 1.0;
+echo  $green
+echo 'Harap Sabar ...Dikit Lagi....ngecrot...';
+sleep 2.0;
+echo  $red
+echo "Loading..............91%.......................";
+sleep 1.0;
+clear
+echo  $red
+echo 'Harap Sabar bgst...96%... ';
+sleep 1.0;
+clear
+echo  $red
+echo 'Prossesing 98%........................';
+sleep 0.9;
+clear
+echo  $green
+echo 'Loading ..............................';
+sleep 2.3;
+clear
+echo  $red
+echo 'Prossesing 99%';
+sleep 1.0;
+clear
+echo  $green
+echo "dikit lagi ngecrot....................................";
+sleep 0.7;
+clear
+echo  $red
+echo "Sedang Memulai Project........................";
+sleep 1.0;
+clear
+echo  $green
+echo '100%................';
+sleep 0.9;
+clear
+echo  $green
+echo 'Sukses!.................................';
+sleep 0.9;
+clear
+echo  $red
+echo 'Nah Gitu Dong Jadi orang sabar........................';
+sleep 1.0;
+clear
+echo  $red
+echo  "AWOKAWOKAWOK";
+sleep 0.5;
+clear
+echo  $green
+echo "================[MEMULAI]================";
+sleep 2.1;
+clear
+echo
+echo  """
+                   
+|======================[PULSA GRATIS WORK]=====================|                                                
+   _| || |_        
+  |_  ..  _|     \033[1;32mAUTHOR  : MR.N0N4M3\033[1;36m
+  |_      _|     \033[1;31mContact : 081291126318\033[1;36m
+    |_||_|       \033[1;31mMy TEAM : Glaxy Cyber Anonymous\033[1;36m
+
+|===========================[WELCOME]!=========================|
+"""
+
+
+
+echo "\033[0;32mMASUKAN NOMOR ANDA : "
+read anda
+echo "\033[0;32mMASUKAN NOMOR YANG INGIN DI KIRIM PULSA : "
+read korban
+echo "\033[0;32mJumlah Pulsa Yang Ingin Dikirim : "
+read pulsa
+echo
+echo
+clear
+echo  $white
+echo 'mulai mengirim......';
+sleep 2.0;
+clear
+echo  $cyan
+echo 'modal makannya tod!!...####... Prosses mengirim.. ###...';
+sleep 3.0;
+clear
+echo  $cyan
+echo 'Sabar Tod!.............................';
+sleep 2.0;
+echo  $white
+echo "Processing mengirim...50%";
+sleep 1.0;
+clear
+echo  $cyan
+echo  'Udah Di Bilangin Sabar Anjenk...............';
+sleep 2.0;
+echo  $white
+echo 'Processing 80%...';
+sleep 1.0;
+clear
+echo  $cyan
+echo 'Loading.....mengirim....88%.......................';
+sleep 1.0;
+echo  $green
+echo 'Sabar ...Mau gratisnya....Doank Gblk...';
+sleep 2.0;
+echo  $white
+echo "Loading..............91%.......................";
+sleep 1.0;
+clear
+echo  $green
+echo 'Harap Sabar bgst...96%... ';
+sleep 1.0;
+clear
+echo  $cyan
+echo 'Prossesing mengirim..98%........................';
+sleep 0.9;
+clear
+echo  $white
+echo 'Loading ..............................';
+sleep 2.3;
+clear
+echo  $cyan
+echo 'Prossesing mengirim...99%';
+sleep 1.0;
+clear
+echo  $green
+echo "dikit lagi bgsd....................................";
+sleep 0.7;
+clear
+echo  $cyan
+echo "Sedang mengirim........................";
+sleep 1.0;
+clear
+echo  $white
+echo '100%................';
+sleep 0.9;
+clear
+echo  $green
+echo 'Sukses Mengirim Pulsa.................................';
+sleep 0.9;
+clear
+echo  $white
+echo 'sedang proses transaksi........................';
+sleep 1.0;
+clear
+echo  $cyan
+echo  "#Makan Tuh Pulsa Tod!!#";
+sleep 3.3;
+clear
+echo   $yellow
+echo "$korban, SUKSES MENGIRIM PULSA" 
+echo "JUMLAH PULSA YANG KE KIRIM : $pulsa"
+echo   $green
+echo "PULSA AKAN DATANG DALAM WAKTU"
+echo "¤~~=>5 - 10 Menit<=~~¤"
